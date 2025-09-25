7c9a961aa2f9f4d6805d3e5ab13d6fc2460e1e1e
bagaspcr <bagas24ti@mahasiswa.pcr.ac.id>
2025-09-25 10:18:55 +0700

    First commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\MahasiswaController;
+
+Route::get('/mahasiswa', function () {
+    return 'Hallo Mahasiswa';
+});
+
+Route::get('/mahasiswa/{param1}',
+[MahasiswaController::class, 'show']) ;
+
+Route::get('/about', function () {
+    return view ('halaman-about');
+});
+
+Route::get('/nama/{param1}', function ($param1) {
+    return 'Nama saya: '.$param1;
+});
+
+Route::get('/nim/{param1?}', function ($param1) {
+    return 'nim saya: '.$param1;
+});
+
+Route::get('/detail', function () {
+    return view ('halaman-mahasiswa-detail');
+});
+
+Route::get('/profil', function () {
+    return view ('halaman-mahasiswa-profil');
+});
